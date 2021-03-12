3431818c0f488bba1ee28caf2cbdd66a211207e2
Florian Herrengt <contact@florianherrengt.com>
2021-03-12 15:29:20 +0000

    add test run env var

--- server/src/setupTests.js
+++ server/src/setupTests.js
@@ -3,12 +3,14 @@ process.env.HASURA_URL = 'HASURA_URL';
 process.env.GITHUB_CLIENT_ID = 'GITHUB_CLIENT_ID';
 process.env.GITHUB_CLIENT_SECRET = 'GITHUB_CLIENT_SECRET';
 process.env.JWT_SECRET = 'JWT_SECRET';
-process.env.DATABASE_URL = 'postgres://localhost:5432/webhooklogstest';
+process.env.DATABASE_URL =
+    process.env.TEST_DATABASE_URL ||
+    'postgres://localhost:5432/webhooklogstest';
 process.env.STRIPE_KEY = 'STRIPE_KEY';
 process.env.STRIPE_SECRET = 'STRIPE_SECRET';
 process.env.STRIPE_PRICE_ID = 'STRIPE_PRICE_ID';
 process.env.DOMAIN = 'DOMAIN';
-process.env.REDIS_URL = 'redis://127.0.0.1';
+process.env.REDIS_URL = process.env.TEST_REDIS_URL || 'redis://127.0.0.1';
 process.env.ADMIN_SECRET = 'ADMIN_SECRET';
 process.env.SERVE_STATIC_FILES = 'false';
 process.env.MATRIX_TOKEN = 'MATRIX_TOKEN';
